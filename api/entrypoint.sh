54ec9fd9ac8e265c007e7b368d9cbe20d0f51993
Petro Davybida <petdavy1991@gmail.com>
2022-09-23 23:09:28 +0300

    split config to dev and prod

---
+++ api/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Starting the application..."
+
+#check if the database is running
+# if [ -z "$(docker ps -q -f name=vik-schedule-dev)" ]; then
+#     echo "Database is not running"
+#     exit 1
+# fi
+
+#check if migrations dir exists
+if [ ! -d "migrations" ]; then
+    echo "Migrations directory does not exist"
+    flask db init
+fi
+
+#check if migrations are up to date
+if [ -z "$(flask db current)" ]; then
+    echo "Migrations are not up to date"
+    flask db migrate
+    flask db upgrade
+fi
+
+#run the app
+flask run --host 0.0.0.0 --port 5000
--- api/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker container run \
---rm \
--e FLASK_APP=/usr/src/app/startapp.py -e FLASK_DEBUG=1 \
---mount type=bind,source=$(pwd),target=/usr/src/app \
--p 5000:5000 \
-vik-schedule
